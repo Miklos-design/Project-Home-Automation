2c6cb4ea9f9587d357f22c4faa2b8a93a539ebe0
Miki-design <73119146+Miklos-design@users.noreply.github.com>
2024-08-04 23:42:20 +0100

    UI wip plus minor changes to the structure, want to separate the colours to make it more readable

--- homesense/lib/pages/home_page.dart
+++ homesense/lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import '../utils/colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,10 +10,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List _MySmartDevices = [
+    ["Living Room", "assets/living_room.png", true],
+    ["Bed Room", "assets/bed_room.png", true],
+    ["Shed", "assets/kitchen.png", true],
+    ["Lounge", "assets/bathroom.png", true],
+    ["Garage", "assets/garage.png", true],
+    ["Blinds", "assets/garden.png", true],
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 96, 98, 111),
+      backgroundColor: Color.fromARGB(255, 90, 212, 249),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -25,7 +35,7 @@ class _HomePageState extends State<HomePage> {
                   Icon(
                     Icons.menu,
                     size: 50,
-                    color: Colors.white38,
+                    color: Color.fromARGB(129, 42, 42, 42),
                   ),
                   Icon(
                     Icons.person,
@@ -41,18 +51,37 @@ class _HomePageState extends State<HomePage> {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 40.0),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text("Wellcome Home"),
                   Text(
                     "Some info",
                     style: TextStyle(
-                        color: Colors.white,
+                        color: Color.fromARGB(255, 255, 255, 255),
                         fontSize: 30,
                         fontWeight: FontWeight.bold),
                   )
                 ],
               ),
-            )
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: Text("Smart Devices"),
+            ),
+            Expanded(
+                child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2),
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20)),
+                      );
+                    }))
           ],
         ),
       ),
