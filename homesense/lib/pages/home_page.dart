9632e796da06190755a2e75bf0e1cb0238319fbb
Miki-design <73119146+Miklos-design@users.noreply.github.com>
2024-08-04 19:06:01 +0100

    Added widgets and icons to the mobile app menu

---
+++ homesense/lib/pages/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 96, 98, 111),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.menu,
+                    size: 50,
+                    color: Colors.white38,
+                  ),
+                  Icon(
+                    Icons.person,
+                    size: 50,
+                    color: Colors.white38,
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text("Wellcome Home"),
+            Text("Some info")
+          ],
+        ),
+      ),
+    );
+  }
+}
