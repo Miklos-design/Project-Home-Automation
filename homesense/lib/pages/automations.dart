17aba9c2e2122e976275f24a5ced195c3d7ecc18
Miki-design <73119146+Miklos-design@users.noreply.github.com>
2024-08-23 01:22:18 +0100

    added automation page, some more tests and refactored

---
+++ homesense/lib/pages/automations.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Automations extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Automation"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Text("Automation Settings Here"),
+      ),
+    );
+  }
+}
